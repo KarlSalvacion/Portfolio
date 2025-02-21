19d175edf1c66e1304016097242bbe99e6654e6d
Karl Vincent <karl_vincent_salvacion@dlsl.edu.ph>
2025-02-21 23:06:46 +0800

    Added More Components and Finishing Details

--- Styles Components/StylesHMP.js
+++ Styles Components/StylesHMP.js
@@ -1,13 +1,22 @@
+import { cloneElement } from "react";
 import { StyleSheet } from "react-native";
 
 const stylesHMP = StyleSheet.create({
-    //TOP AREA CONTAINER
+    //TOP AREA 
     
     darkModeToggler: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
         marginRight: 10,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+    },
+
+    darkDarkModeToggler: {
+        shadowColor: 'rgb(219, 167, 36)',
     },
 
     darkModeIcon: {
@@ -34,12 +43,9 @@ const stylesHMP = StyleSheet.create({
     topArea: {
         height: 90,
         width: '100%',
-        backgroundColor: 'rgb(244, 239, 235)',
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 15 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        padding: 10,        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        padding: 10,        
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
         position: 'absolute', 
@@ -50,7 +56,7 @@ const stylesHMP = StyleSheet.create({
     },
 
     darkTopArea: {
-        backgroundColor: 'rgb(37, 34, 32)',
+        backgroundColor: 'transparent',
         
     },
 
@@ -59,10 +65,20 @@ const stylesHMP = StyleSheet.create({
         fontFamily: 'RubikMonoOne',
         fontSize: 16,
         marginLeft: 10,
+        backgroundColor: 'rgb(253, 253, 253)',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 100,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
     },
 
     darkTopTextHeader: {
-        color: 'rgb(219, 167, 36)',
+        color: 'rgb(31, 31, 33)',
+        backgroundColor: 'rgb(219, 167, 36)',
+        shadowColor: 'rgb(219, 167, 36)',
     },
     
     nameHeaderArea: {
@@ -73,7 +89,7 @@ const stylesHMP = StyleSheet.create({
         color: 'black',
     },
 
-    //CONTENT AREA CONTAINER
+    //CONTENT AREA 
 
     scrollView: {
         flex: 1,
@@ -87,8 +103,6 @@ const stylesHMP = StyleSheet.create({
     imageContainer: {
         backgroundColor: 'rgb(244, 239, 235)',
         width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
         paddingVertical: 20,
         height: 750,
     },
@@ -98,65 +112,389 @@ const stylesHMP = StyleSheet.create({
     },  
 
     imageFormat: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: 250,
+        height: 250,
+        borderRadius: 220,
     },
      
     imageArea: {
-        justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 50,
         marginBottom: 20,
+        shadowColor: 'rgb(20, 22, 58)',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+    },
+
+    fullNameBA: {
+        fontFamily: 'Rubik600SB',
+        fontSize: 22,
+        color: 'rgb(20, 22, 58)',
+    },
+
+    darkFullNameBA: {
+        color: 'rgb(246, 183, 25)',
+    },
+
+    bioText1: {
+        color: 'rgb(20, 22, 58)',
+        fontFamily: 'Rubik400R',
+        fontSize: 14,
+    },
+
+    darkBioText1: {
+        color: 'rgb(233, 231, 231)',
+    },
+
+    bioText2: {
+        color: 'rgb(20, 22, 58)',
+        fontFamily: 'Rubik500M',
+        fontSize: 14,
+    },
+
+    darkBioText2: {
+        color: 'rgb(233, 231, 231)',
+    },
+
+    welcomeText: {
+        color: 'rgb(49, 47, 47)',
+        fontFamily: 'Rubik700B',
+        fontSize: 17
+    },
+
+    darkWelcomeText: {
+        color: 'rgb(233, 233, 231)',
     },
 
     bioArea: {
         justifyContent: 'center',
         alignItems: 'center',
+        marginHorizontal: 30,
+        gap: 0,
     },
 
-    buttonSeeProjects: {
-        backgroundColor: 'green',
+    linksContainer: {
+        backgroundColor: 'transparent',
+        height: 300,
+    },
+      
+    linksIcons: {
+        flexDirection: 'row',
+        marginLeft: 10,
+        width: 110,
+    }, 
+
+    iconsArea: {
+        gap: 30, 
+        marginTop: 130,
+        marginLeft: 20,
+    },
+      
+    iconsDesign: {
+        fontSize: 25,
+    },
+      
+    darkIconDesign: {
+        color: 'rgb(219, 167, 36)',
+    },
+      
+    arrowDownArea: {
+        position: 'absolute',
+        bottom: -40,
+        right: 20,
+    },
+      
+    buttonSeeAboutMe: {
         paddingVertical: 10,
         paddingHorizontal: 20,
-        marginTop: 10,
         flexDirection: 'row',
         alignItems: 'center',
         borderRadius: 5,
     },
+    
+    arrowDownIcon: {
+        color: 'rgb(31, 31, 33)',
+        fontSize: 40,
+    },
+
+    darkArrowDownIcon: {
+        color: 'rgb(219, 167, 36)',
+    },
+
+    //ABOUT ME
+    
+    aboutMeContainer: {
+        backgroundColor: 'transparent',
+        height: 900,
+        width: '100%',
+        alignItems: 'center',
+    },
+
+    aboutMeHeaderText: {
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 25,
+        fontFamily: 'Rubik500M',
+        marginTop: 100,
+        backgroundColor: 'rgb(20, 22, 58)',
+        color: 'rgb(253, 253, 253)',
+        paddingVertical: 3,
+        width: 150,
+        borderRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+    },
+
+    darkAboutMeHeaderText: {
+        backgroundColor: 'rgb(219, 167, 36)',
+        shadowColor: 'rgb(219, 167, 36)',
+        color: 'rgb(31, 31, 33)',
+    }, 
+    
+    aboutMeContent: {
+        marginHorizontal: 30,
+        marginTop: 30,
+        flexDirection: 'column',
+    },
+
+    aboutMeContentBioArea: {
+        backgroundColor: 'transparent',
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        width: 390,
+        marginHorizontal: 30,
+        borderWidth: 2,
+        borderColor: 'rgb(20, 22, 58)',
+        alignSelf: 'center',
+    },
+    
+    darkAboutMeContentBioArea: {
+        backgroundColor: 'transparent',
+        borderColor: 'rgb(219, 167, 36)',
+    },
+
+    aboutMeBio: {
+        fontSize: 16,
+        color: 'rgb(20, 22, 58)',
+        fontFamily: 'Rubik500M',
+        textAlign: 'justify',
+
+    },
+
+    darkAboutMeBio: {
+        color: 'rgb(219, 167, 36)',
+        backgroundColor: 'tranparent',
+    },
+    
+    skillsContainer: {
+        alignItems: 'center',
+    },
+
+    skillIcon: {
+        color: 'rgb(20, 22, 58)',
+        fontSize: 20,
+    },
+
+    darkSkillIcon: {
+        color: 'rgb(219, 167, 36)',
+    },  
+
+    skillsList: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',  
+        justifyContent: 'space-between',
+        marginVertical: 0,
+        width: '80%', 
+    },
+
+    skillsHeaderText: {
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 25,
+        fontFamily: 'Rubik500M',
+        backgroundColor: 'rgb(20, 22, 58)',
+        color: 'rgb(253, 253, 253)',
+        paddingVertical: 3,
+        width: 150,
+        borderRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        marginVertical: 30,
+    },
+
+    darkSkillsHeaderText: {
+        backgroundColor: 'rgb(219, 167, 36)',
+        color: 'rgb(31, 31, 33)',
+        shadowColor: 'rgb(219, 167, 36)',
+    },
+
+    skillsContent: {
+        backgroundColor: 'transparent',
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        width: '48%', 
+        alignItems: 'center',  
+        justifyContent: 'center',  
+        borderWidth: 2,
+        borderColor: 'rgb(20, 22, 58)',
+        marginBottom: 10,  
+    },
+
+    darkSkillsContent: {
+        backgroundColor: 'transparent',
+        borderColor: 'rgb(219, 167, 36)',
+    },
+    
+    skillsText: {
+        textAlign: 'center',
+        color: 'rgb(20, 22, 58)',
+        fontFamily: 'Rubik500M',
+
+    },
+
+    darkSkillsText: {
+        color: 'rgb(219, 167, 36)',
+    },
+    
+    linksIconsAboutMe: {
+        marginLeft: 0,
+        width: 100,
+        alignItems: 'center',
+    },
+
+    iconsAreaAboutMe: {
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        alignSelf: 'center',
+        gap: 0,
+        marginTop: 20,
+        marginLeft: 0,
+    },
+
+    arrowDownAreaAboutMe: {
+        position: 'absolute',
+        bottom: -120,
+        right: 20,
+    },
+    
+    //PROJECTS
 
     projectsContainer: {
         width: '100%',
-        backgroundColor: 'rgb(231,223,3)',
+        backgroundColor: 'transparent',
         paddingVertical: 20,
         paddingHorizontal: 10,
+        height: 1300,
+        alignItems: 'center',
+        alignSelf: 'center',
     },  
 
     projectHeader: {
-        fontSize: 24,
-        fontWeight: 'bold',
         textAlign: 'center',
-        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 25,
+        fontFamily: 'Rubik500M',
+        backgroundColor: 'rgb(20, 22, 58)',
+        color: 'rgb(253, 253, 253)',
+        paddingVertical: 3,
+        width: 190,
+        borderRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        marginVertical: 30,
+
+    },
+
+    darkProjectHeader: {
+        backgroundColor: 'rgb(219, 167, 36)',
+        color: 'rgb(31, 31, 33)',
+        shadowColor: 'rgb(219, 167, 36)',
+    },  
+
+    projectFlatlist: {
+        gap: 0,
     },
 
     projectItem: {
-        backgroundColor: 'white',
-        padding: 15,
+        backgroundColor: 'transparent',
+        padding: 10,
+        width: 300,
+        alignItems: 'center',
         marginVertical: 5,
         borderRadius: 10,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 4,
+        borderWidth: 2,
+        borderColor: 'rgb(20, 22, 58)',
+        gap: 5,
+    },
+
+    darkProjectItem: {
+        borderColor: 'rgb(219, 167, 36)',
+        backgroundColor: 'transparent',
     },
 
     projectTitle: {
         fontSize: 18,
         fontWeight: 'bold',
+        color: 'rgb(20, 22, 58)',
+    },
+
+    darkProjectTitle: {
+        color: 'rgb(219, 167, 36)',
     },
 
     projectDescription: {
         fontSize: 14,
-        color: 'gray',
+        color: 'rgb(67, 67, 66)',
+        textAlign: 'center',
+    },
+
+    darkProjectDescription: {
+        color: 'rgb(209, 208, 205)',
+    }, 
+
+    projectImage: {
+        width: 200,
+        height: 100,
+    },
+
+    githubText: {
+        color: 'rgb(20, 22, 58)',
+        fontFamily: 'Rubik500M'
+    },  
+
+    darkGitHubText: { 
+        color: 'rgb(208, 176, 33)',
+    },
+
+    arrowDownAreaProjects: {
+        position: 'absolute',
+        bottom: 10,
+        right: 20,
     },
 });
 
